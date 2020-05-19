372b1f881aad3eae05f526fc17a5c3d648086517
mathisBenny <matnice06@hotmail.com>
2020-05-18 20:01:51 -0400

    Ajout de la classe Blackjack

---
+++ BlackJack.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack {
+    public class Blackjack {
+        public Blackjack(){
+            }
+
+        public void CreerPartie() {
+
+        }
+
+        public void JoindrePartie() {
+
+        }
+
+        public void Demarrer() {
+
+        }
+    }
+}
