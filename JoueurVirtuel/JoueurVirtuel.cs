e2b208a8be7b41d3c16d7967fe9cbbda3337503d
Anthony Chamberland <anthochamberland19@icloud.com>
2020-06-01 22:06:59 -0400

    Supression de la bibliothèque de classes JoueurVirutel
    Celle-ci étant remplacée par IA

--- JoueurVirtuel/JoueurVirtuel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Blackjack {
-    /// <summary>Classe responsable des actions de joueurs virtuel.</summary>
-    public static class JoueurVirtuel {
-        /// <summary>Évalue si le joueur virtuel doit tirer ou rester.</summary>
-        /// <param name="total">Total du joueur virtuel.</param>
-        /// <returns>Retourne si le joueur virtuel doit tirer (True) ou rester (False)</returns>
-        public static bool Action(int total) => total < 17;
-    }
-}
